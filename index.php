c98512d319b3e6cb821ab65273fe1223cf406fcb
“Fenix36GIT” <pabloaixacliment2007@gmail.com>
2024-10-23 13:44:39 +0200

    Nuevos ficheros

---
+++ index.php
@@ -0,0 +1,6 @@
+echo 'En enero, cierra la puerta y enciende la brasa';
+echo 'Las heladas de enero hacen florecer los almendros';
+echo 'Por febrero flor en el almendro';
+echo 'Agua de enero todo el año va bien';
+echo 'Por febrero, Carnestoltes el primero';
+echo 'Mas tarde o mas pronto carnaval en de febrero';
